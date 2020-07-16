b4061d7e3f1d96eeb316337505de0961add07e45
Ariel-Sanguin <66977702+Ariel-Sanguin@users.noreply.github.com>
2020-07-16 03:53:57 -0300

    v200716.0.9

---
+++ Especiales/ejEsp_04/articulosJSON.js
@@ -0,0 +1,16 @@
+var articulos = '{"opciones":' + '[' + '{"codigo":"001","famila":"Frutas secas","UM":"KG","Descripcion":"Almendras", "Fecha":"17/07/2020","Stock":"25"},'+
+'{"codigo":"002","famila":"Frutas secas", "UM":"KG", "Descripcion":"Nueces", "Fecha":"17/07/2020", "Stock":"5"},'+
+'{"codigo":"003", "famila":"Frutas", "UM":"KG", "Descripcion":"Manzana", "Fecha":"14/07/2020", "Stock":"40"},' +
+'{"codigo":"004", "famila":"Conserva de pescado", "UM":"KG","Descripcion":"Atun", "Fecha":"14/07/2020", "Stock":"15"},' +
+'{"codigo":"005", "famila":"conserva de frutas", "UM":"KG", "Descripcion":"Duraznos", "Fecha":"14/07/2020", "Stock":"17"},'+
+'{"codigo":"001","famila":"Frutas secas","UM":"KG","Descripcion":"Almendras", "Fecha":"17/07/2020","Stock":"25"},'+
+'{"codigo":"002","famila":"Frutas secas", "UM":"KG", "Descripcion":"Nueces", "Fecha":"17/07/2020", "Stock":"5"},'+
+'{"codigo":"003", "famila":"Frutas", "UM":"KG", "Descripcion":"Manzana", "Fecha":"14/07/2020", "Stock":"40"},' +
+'{"codigo":"004", "famila":"Conserva de pescado", "UM":"KG","Descripcion":"Atun", "Fecha":"14/07/2020", "Stock":"15"},' +
+'{"codigo":"005", "famila":"conserva de frutas", "UM":"KG", "Descripcion":"Duraznos", "Fecha":"14/07/2020", "Stock":"17"},'+
+'{"codigo":"001","famila":"Frutas secas","UM":"KG","Descripcion":"Almendras", "Fecha":"17/07/2020","Stock":"25"},'+
+'{"codigo":"002","famila":"Frutas secas", "UM":"KG", "Descripcion":"Nueces", "Fecha":"17/07/2020", "Stock":"5"},'+
+'{"codigo":"003", "famila":"Frutas", "UM":"KG", "Descripcion":"Manzana", "Fecha":"14/07/2020", "Stock":"40"},' +
+'{"codigo":"004", "famila":"Conserva de pescado", "UM":"KG","Descripcion":"Atun", "Fecha":"14/07/2020", "Stock":"15"},' +
+'{"codigo":"005", "famila":"conserva de frutas", "UM":"KG", "Descripcion":"Duraznos", "Fecha":"14/07/2020", "Stock":"17"}'+ ']'
++ '}';
---
+++ Especiales/ejEsp_04/opcionesJSON.js
@@ -0,0 +1,6 @@
+var options = '{"opciones":' +'['+
+            '{"id": "001", "familia": "Frutos secos"},'+
+            '{"id": "002", "familia": "Conservas de fruta"},'+
+            '{"id": "003", "familia": "Conservas de verdura"},'+
+            '{"id": "004", "familia": "Conservas de legumbres"},'+
+            '{"id": "005", "familia": "Especias"}'+']'+'}';
